c2b85016176f9016e92fac146810c93ccee11ab1
Denis Lusson <denis.lusson@epita.fr>
2021-11-05 15:23:18 +0100

    add first cmake and main

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <CLI/CLI.hpp>
+
+
+int main(int argc, char** argv) {
+    (void) argc;
+    (void) argv;
+
+    std::string filename = "output.png";
+    std::string mode = "GPU";
+
+    CLI::App app{"code"};
+    app.add_option("-o", filename, "Output image");
+    app.add_set("-m", mode, {"GPU", "CPU"}, "Either 'GPU' or 'CPU'");
+    CLI11_PARSE(app, argc, argv);
+
+    // Rendering
+    if (mode == "CPU")
+    {
+        printf("CPU\n");
+    }
+    else if (mode == "GPU")
+    {
+        printf("GPU\n");
+    }
+
+}
