f3c945c4f033c024a3dda2761479fb7eebdca64b
Hugo Rosenkranz <hugo.rosenkranz@epita.fr>
2021-11-16 12:07:56 +0100

    float gpool

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <CLI/CLI.hpp>
 #include <opencv2/opencv.hpp>
 #include <iostream>
-
+#include <stdio.h>
 
 
 int main(int argc, char** argv) {
@@ -47,4 +47,39 @@ int main(int argc, char** argv) {
     cv::Mat soby1;
     cv::Sobel(img_grayscale, soby1, -1, 0, 1);
     cv::imwrite("../output/sobely.jpg", soby1);
+
+    //cv::Mat abs_grad_x, abs_grad_y;
+    //cv::convertScaleAbs(sobx1, abs_grad_x);
+    //cv::convertScaleAbs(soby1, abs_grad_y);
+
+    //printf("%d %d\n", sobx1.at<uint8_t>(0,0), sobx1.at<uint8_t>(1478, 2116));
+
+    int pool_size = 31;
+    cv::Size s = sobx1.size();
+    int patchs_y = s.height/pool_size;
+    int patchs_x = s.width/pool_size;
+    std::cout << patchs_y << " " << patchs_x << std::endl;
+    cv::Mat response(cv::Size(patchs_y, patchs_x), CV_64FC1, cv::Scalar(0));
+
+    
+
+    // crop = grad_img[:pool_size*patchs_y, :pool_size*patchs_x]
+    // features_patchs = view_as_blocks(crop, block_shape=(pool_size, pool_size))
+    for (int ii = 0; ii < patchs_y; ii++) {
+        for (int jj = 0; jj < patchs_x; jj++) {
+            unsigned int sum = 0;
+            int index_y = ii * pool_size;
+            int index_x = jj * pool_size;
+            for (int i = index_y; i < index_y+pool_size; i++) {
+                for (int j = index_x; j < index_x+pool_size; j++) {
+                    sum += sobx1.at<uint8_t>(i, j);
+                }
+            }
+            float avg = (1.0*sum)/(pool_size*pool_size);
+            //std::cout << avg << std::endl;
+            response.at<float>(ii, jj) = avg;
+        }
+    }
+    
+    cv::imwrite("../output/pool_sobx1.jpg", response);
 }
