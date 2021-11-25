1f9044f01b1355ccf783fc51f247efea1c85926b
Hugo Rosenkranz <hugo.rosenkranz@epita.fr>
2021-11-25 19:08:42 +0100

    Sobel filter x

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <CLI/CLI.hpp>
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include<vector>
+#include<algorithm>
 #include "process.hpp"
 
 int main(int argc, char** argv) {
@@ -35,8 +37,16 @@ int main(int argc, char** argv) {
 
     unsigned char *buffer = img_vec.data();
 
-    sobel_filter(buffer, img.cols, img.rows, img.cols);
 
-    cv::Mat output = cv::Mat(img.rows, img.cols, CV_8U, buffer);
+    float *sobel_x = (float*)calloc(img.rows * img.cols, sizeof(float));
+
+    sobel_filter(buffer, sobel_x, img.cols, img.rows, img.cols);
+
+    std::vector<float> v(sobel_x, sobel_x + img.rows * img.cols);
+    float max = *std::max_element(v.begin(), v.end());
+
+    transform(v.begin(), v.end(), v.begin(), [max](float &c){ return 255*c/max; });
+
+    cv::Mat output = cv::Mat(img.rows, img.cols, CV_32FC1, v.data());
     cv::imwrite("../output_gpu/test.jpg", output);
 }
--- src/process.cu
+++ src/process.cu
@@ -1,42 +1,47 @@
 #include "process.hpp"
 #include <stdio.h>
 
-__global__ void compute_filter(const unsigned char* in, unsigned char *out, int width, int height, int r, int sIn, int sOut) {
+__global__ void compute_filter(const unsigned char* in, float *out, int width, int height, int sIn, int sOut) {
 
-  int x = blockDim.x * blockIdx.x + threadIdx.x;
-  int y = blockDim.y * blockIdx.y + threadIdx.y;
+    float sobel_x[3][3] = {{-1.0, 0.0, 1.0}, {-2.0, 0.0, 2.0}, {-1.0, 0.0, 1.0}};
+    int r = 1;
 
-  if (x < r || x >= width - r) return;
-  if (y < r || y >= height - r) return;
+    int x = blockDim.x * blockIdx.x + threadIdx.x;
+    int y = blockDim.y * blockIdx.y + threadIdx.y;
 
-  int sum = 0;
-  for (int kx = -r; kx <= r; kx++) {
-      for (int ky = -r; ky <= r; ky++) {
-          sum += in[((y + ky) * sIn) + (x + kx)];
-      }
-  }
-  out[x + y * sOut] = sum / ((2*r+1) * (2*r+1));
+    if (x < r || x >= width - r) return;
+    if (y < r || y >= height - r) return;
+
+    float sum = 69.0;
+    for (int kx = -r; kx <= r; kx++) {
+        for (int ky = -r; ky <= r; ky++) {
+            float pixel = 1.0*in[((y + ky) * sIn) + (x + kx)];
+            sum += sobel_x[ky+r][kx+r] * pixel;
+        }
+    }
+
+    out[x + y * sIn] = (sum > 0) ? sum : -sum;
 }
 
-void sobel_filter(unsigned char* buffer, int width, int height, int stride) {
+void sobel_filter(unsigned char* buffer, float *filter_output, int width, int height, int stride) {
     cudaError_t rc = cudaSuccess;
 
     // Allocate device memory
     unsigned char*  devIn;
-    unsigned char* devOut;
+    float* devOut;
     size_t pitchIn, pitchOut;
 
     rc = cudaMallocPitch(&devIn, &pitchIn, width * sizeof(char), height);
     if (rc)
-        printf("Fail buffer allocation");
+        printf("Fail buffer allocation\n");
 
     rc = cudaMemcpy2D(devIn, pitchIn, buffer, stride, width, height, cudaMemcpyHostToDevice);
     if (rc)
-        printf("Unable to copy buffer back to memory");
+        printf("Unable to copy buffer back to memory\n");
 
-    rc = cudaMallocPitch(&devOut, &pitchOut, width * sizeof(char), height);
+    rc = cudaMallocPitch(&devOut, &pitchOut, width * sizeof(float), height);
     if (rc)
-        printf("Fail buffer allocation");
+        printf("Fail buffer allocation\n");
 
     printf("%d, %d, %d, %d\n", width, height, pitchIn, pitchOut);
 
@@ -47,16 +52,17 @@ void sobel_filter(unsigned char* buffer, int width, int height, int stride) {
 
         dim3 dimBlock(bsize, bsize);
         dim3 dimGrid(w, h);
-        compute_filter<<<dimGrid, dimBlock>>>(devIn, devOut, width, height, 5, pitchIn, pitchOut);
+        compute_filter<<<dimGrid, dimBlock>>>(devIn, devOut, width, height, pitchIn, pitchOut);
         cudaDeviceSynchronize();
 
         if (cudaPeekAtLastError())
-            printf("compute_filter Error");
+            printf("compute_filter Error\n");
 
     }
 
     // Copy back to main memory
-    rc = cudaMemcpy2D(buffer, stride, devOut, pitchOut, width * sizeof(char), height, cudaMemcpyDeviceToHost);
+    rc = cudaMemcpy2D(filter_output, stride * sizeof(float), devOut, pitchOut,
+                        width * sizeof(float), height, cudaMemcpyDeviceToHost);
     if (rc)
-        printf("Unable to copy buffer back to memory");
+        printf("Unable to copy buffer back to memory\n");
 }
